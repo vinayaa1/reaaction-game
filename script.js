52d7e9d014f65646ded1b8b4718e93c05596f45b
Vinaya Ayinampudi <145865208+vinayaa1@users.noreply.github.com>
2026-04-03 23:10:01 -0700

    added themes, leaderboard, work in progress thought- will update later

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let startTime = 0;
 let timeout = null;
 let gameState = "idle";
 let reactionTimes = [];
-let round = 0
+let round = 0;
+let theme = "cuteness";
 
 const maxRounds = 5;
 const box = document.getElementById("box");
@@ -19,6 +20,7 @@ function resetGame() {
     box.style.display = "none";
     result.textContent = "";
     message.textContent = "click start to begin!!! (you will play 5 rounds, click the square as soon as it turns green, your average score will be given at the end!"
+    playClick();
 }
 
 function startRound(){
@@ -32,6 +34,7 @@ function startRound(){
     timeout = setTimeout(() => {
         box.style.background = "#7ae582";
         message.textContent = "click nowww";
+        playSuccess();
         startTime = Date.now();
         gameState = "ready";
         }, delay);
@@ -52,6 +55,28 @@ function endGame() {
 
   gameState = "idle";
   box.style.display = "none";
+
+  leaderboard.push({
+    avg: Math.round(avg),
+    best: best,
+    date: new Date().toLocaleTimeString()
+  })
+
+  leaderboard.sort((a,b) => a.best - b.best);
+  leaderboard = leaderboard.slice(0,5);
+
+  localStorage.setItem("leaderboard", JSONstringify(leaderboard));
+  renderLeaderboard();
+
+}
+
+function renderLeaderboard() {
+  const lb = document.getElementById("leaderboard");
+
+  lb.innerHTML = "<h3>🏆 Leaderboard</h3>" +
+    leaderboard.map((s, i) =>
+      `<p>${i + 1}. ${s.best}ms (avg ${s.avg}ms)</p>`
+    ).join("");
 }
 
 startBtn.onclick = () => {
@@ -64,7 +89,7 @@ box.onclick = () => {
   if (gameState === "waiting") {
     clearTimeout(timeout);
     message.textContent = "too early...";
-
+    playFail();
     setTimeout(() => {
       startRound();
     }, 1000);
@@ -90,3 +115,17 @@ box.onclick = () => {
     }, 800);
   }
 };
+
+const themeBtn = document.getElementById("themeToggle");
+
+themeBtn.onclick = () => {
+  if (theme === "cuteness") {
+    theme = "dark";
+    document.body.className = "dark";
+  } else {
+    theme = "cuteness";
+    document.body.className = "cuteness";
+  }
+};
+
+let leaderboard = JSON.parse(localStorage.getItem("leaderboard")) || [];
